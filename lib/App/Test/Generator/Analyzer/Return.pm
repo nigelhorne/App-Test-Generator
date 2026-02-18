e5b1c88666096fc89bcb86db2023fd5203e50cf3
Nigel Horne <njh@bandsman.co.uk>
2026-02-18 09:47:31 -0500

    White space

--- lib/App/Test/Generator/Analyzer/Return.pm
+++ lib/App/Test/Generator/Analyzer/Return.pm
@@ -4,44 +4,44 @@ use strict;
 use warnings;
 
 sub new {
-    my ($class) = @_;
-    return bless {}, $class;
+	my $class = $_[0];
+	return bless {}, $class;
 }
 
 sub analyze {
-    my ($self, $method) = @_;
+	my ($self, $method) = @_;
 
-    my $source = $method->source;
+	my $source = $method->source();
 
-    # return $self->{property}
-    if ($source =~ /return\s+\$self->\{(\w+)\}/) {
-        $method->add_evidence(
-            category => 'return',
-            signal   => 'returns_property',
-            value    => $1,
-            weight   => 20,
-        );
-    }
+	# return $self->{property}
+	if ($source =~ /return\s+\$self->\{(\w+)\}/) {
+		$method->add_evidence(
+			category => 'return',
+			signal   => 'returns_property',
+			value    => $1,
+			weight   => 20,
+		);
+	}
 
-    # return $self
-    if ($source =~ /return\s+\$self\b/) {
-        $method->add_evidence(
-            category => 'return',
-            signal   => 'returns_self',
-            weight   => 15,
-        );
-    }
+	# return $self
+	if ($source =~ /return\s+\$self\b/) {
+		$method->add_evidence(
+			category => 'return',
+			signal   => 'returns_self',
+			weight   => 15,
+		);
+	}
 
-    # return constant literal
-    if ($source =~ /return\s+(['"])/) {
-        $method->add_evidence(
-            category => 'return',
-            signal   => 'returns_constant',
-            weight   => 10,
-        );
-    }
+	# return constant literal
+	if ($source =~ /return\s+(['"])/) {
+		$method->add_evidence(
+			category => 'return',
+			signal   => 'returns_constant',
+			weight   => 10,
+		);
+	}
 
-    return;
+	return;
 }
 
 1;
--- lib/App/Test/Generator/Model/Method.pm
+++ lib/App/Test/Generator/Model/Method.pm
@@ -4,131 +4,117 @@ use strict;
 use warnings;
 
 sub new {
-    my ($class, %args) = @_;
+	my ($class, %args) = @_;
 
-    die "name required"   unless defined $args{name};
-    die "source required" unless defined $args{source};
+	die 'name required'   unless defined $args{name};
+	die 'source required' unless defined $args{source};
 
-    my $self = {
-        name          => $args{name},
-        source        => $args{source},
-        parameters    => [],
-        evidence      => [],
-        return_type   => undef,
-        classification=> undef,
-        confidence    => undef,
-    };
+	my $self = {
+		name          => $args{name},
+		source        => $args{source},
+		parameters    => [],
+		evidence      => [],
+		return_type   => undef,
+		classification=> undef,
+		confidence    => undef,
+	};
 
-    return bless $self, $class;
+	return bless $self, $class;
 }
 
 sub name       { $_[0]->{name} }
 sub source     { $_[0]->{source} }
 
 sub return_type {
-    my ($self, $val) = @_;
-    $self->{return_type} = $val if @_ > 1;
-    return $self->{return_type};
+	my ($self, $val) = @_;
+	$self->{return_type} = $val if @_ > 1;
+	return $self->{return_type};
 }
 
 sub classification {
-    my ($self, $val) = @_;
-    $self->{classification} = $val if @_ > 1;
-    return $self->{classification};
+	my ($self, $val) = @_;
+	$self->{classification} = $val if @_ > 1;
+	return $self->{classification};
 }
 
 sub confidence {
-    my ($self, $val) = @_;
-    $self->{confidence} = $val if @_ > 1;
-    return $self->{confidence};
+	my ($self, $val) = @_;
+	$self->{confidence} = $val if @_ > 1;
+	return $self->{confidence};
 }
 
 sub add_evidence {
-    my ($self, %args) = @_;
+	my ($self, %args) = @_;
 
-    push @{ $self->{evidence} }, {
-        category => $args{category},   # return/input/effect
-        signal   => $args{signal},
-        value    => $args{value},
-        weight   => defined $args{weight} ? $args{weight} : 1,
-    };
+	push @{ $self->{evidence} }, {
+		category => $args{category},   # return/input/effect
+		signal   => $args{signal},
+		value    => $args{value},
+		weight   => defined $args{weight} ? $args{weight} : 1,
+	};
 }
 
 sub evidence {
-    my ($self) = @_;
-    return @{ $self->{evidence} };
+	my $self = $_[0];
+	return @{ $self->{evidence} };
 }
 
 sub evidence_ref {
-    my ($self) = @_;
-    return $self->{evidence};
+	my $self = $_[0];
+	return $self->{evidence};
 }
 
 sub resolve_return_type {
-    my ($self) = @_;
+	my $self = $_[0];
 
-    my %score;
+	my %score;
 
-    for my $ev (@{ $self->{evidence} }) {
-        next unless $ev->{category} eq 'return';
+	for my $ev (@{ $self->{evidence} }) {
+		next unless $ev->{category} eq 'return';
 
-        if ($ev->{signal} eq 'returns_property') {
-            $score{property} += $ev->{weight};
-        }
-        elsif ($ev->{signal} eq 'returns_constant') {
-            $score{constant} += $ev->{weight};
-        }
-        elsif ($ev->{signal} eq 'returns_self') {
-            $score{object} += $ev->{weight};
-        }
-    }
+		if ($ev->{signal} eq 'returns_property') {
+			$score{property} += $ev->{weight};
+		} elsif ($ev->{signal} eq 'returns_constant') {
+			$score{constant} += $ev->{weight};
+		} elsif ($ev->{signal} eq 'returns_self') {
+			$score{object} += $ev->{weight};
+		}
+	}
 
-    my ($winner) =
-        sort { ($score{$b}||0) <=> ($score{$a}||0) }
-        keys %score;
+	my ($winner) = sort { ($score{$b}||0) <=> ($score{$a}||0) } keys %score;
 
-    $self->{return_type} = $winner || 'unknown';
+	$self->{return_type} = $winner || 'unknown';
 
-    return $self->{return_type};
+	return $self->{return_type};
 }
 
 sub resolve_confidence {
-    my ($self) = @_;
+	my $self = $_[0];
 
-    my $total = 0;
-    $total += $_->{weight} for @{ $self->{evidence} };
+	my $total = 0;
+	$total += $_->{weight} for @{ $self->{evidence} };
 
-    my $level =
-          $total >= 40 ? 'high'
-        : $total >= 20 ? 'medium'
-        : 'low';
+	my $level = $total >= 40 ? 'high' : $total >= 20 ? 'medium' : 'low';
 
-    $self->{confidence} = {
-        score => $total,
-        level => $level,
-    };
+	$self->{confidence} = { score => $total, level => $level };
 
-    return $self->{confidence};
+	return $self->{confidence};
 }
 
-
 sub resolve_classification {
-    my ($self) = @_;
+	my $self = $_[0];
 
-    if ($self->{return_type} eq 'object') {
-        $self->{classification} = 'chainable';
-    }
-    elsif ($self->{return_type} eq 'property') {
-        $self->{classification} = 'getter';
-    }
-    elsif ($self->{return_type} eq 'constant') {
-        $self->{classification} = 'constant';
-    }
-    else {
-        $self->{classification} = 'unknown';
-    }
+	if ($self->{return_type} eq 'object') {
+		$self->{classification} = 'chainable';
+	} elsif ($self->{return_type} eq 'property') {
+		$self->{classification} = 'getter';
+	} elsif ($self->{return_type} eq 'constant') {
+		$self->{classification} = 'constant';
+	} else {
+		$self->{classification} = 'unknown';
+	}
 
-    return $self->{classification};
+	return $self->{classification};
 }
 
 1;
